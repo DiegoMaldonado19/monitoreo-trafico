24b423ac958e9f9802918d47d4707a85666baa67
Diego Maldonado <dj_maldonado19@hotmail.es>
2025-03-17 00:21:36 -0600

    Adding changes on SQL data script

--- SQL Scripts/Data.sql
+++ SQL Scripts/Data.sql
@@ -6,12 +6,12 @@ INSERT INTO rol (nombre_rol) VALUES ('Supervisor');
 -- Insertar usuarios con contraseñas hasheadas (usando bcrypt, común en Laravel)
 -- Contraseñas: admin1, admin2, monitor1, monitor2, supervisor1, supervisor2
 INSERT INTO usuario (nombre_usuario, contrasena, id_rol) VALUES 
-('admin1', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 1), -- admin1
-('admin2', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 1), -- admin2
-('monitor1', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 2), -- monitor1
-('monitor2', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 2), -- monitor2
-('supervisor1', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 3), -- supervisor1
-('supervisor2', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 3); -- supervisor2
+('admin1', '$2y$10$xoxX0p5L9vkk2ulWHOlVUuPoseMauVoJNhaNE7rVqnuoX6EXmZfK2', 1), -- admin1
+('admin2', '$2y$10$iueY9y4A3a2m8UX8c7umX.qOUbuXYHzfm9ERiGyFKB8RqnzQZmtpy', 1), -- admin2
+('monitor1', '$2y$10$45mUC0LKPyX5lCqSj5c4OuhC4Ci5A6nyM5uYZ7MesFX9pfHmpgKgy', 2), -- monitor1
+('monitor2', '$2y$12$bHKFy5yfwFTpKMuadrjrNesBW1p7LNdO56pwAHwLT6Fz3CmDyRtqG', 2), -- monitor2
+('supervisor1', '$2y$10$o5Ys7CiIKSf55.GtRNwllOIglmbZQIvxblagTHF6cqVuiPQA9DNTm', 3), -- supervisor1
+('supervisor2', '$2y$10$NOcVe.Lts6uguRyPrdBoSeBiZVGtjAXb/KlCRNW/fwwcO7I2wq0IK', 3); -- supervisor2
 
 -- Insertar tipos de calles
 INSERT INTO tipo_calle (nombre_tipo_calle) VALUES ('calle');
